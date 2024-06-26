545812a3e846877c63e3fafd33f6a0469700e940
BBangminjoo <165381002+BBangminjoo@users.noreply.github.com>
2024-06-26 20:47:36 +0900

    Update 조건 문자열.java

--- programmers/0레벨/조건 문자열.java
+++ programmers/0레벨/조건 문자열.java
@@ -1,13 +1,18 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
-
+        
+        // ineq가 >일때 
 		if(ineq.equals(">")) {
+                        // eq가 =일때
 			if(eq.equals("=")) {
+                             // n <= m 이 성립하면 1 , 아니면 0
 			    answer = n >= m ? 1 : 0;
+                        // eq가 !일때
 			}else if(eq.equals("!")) {
 				 answer = n > m ? 1 : 0;
 			}
+                 // ineq가 <일때     
 		}else if(ineq.equals("<")) {
 			if(eq.equals("=")) {
 				 answer = n <= m ? 1 : 0;
