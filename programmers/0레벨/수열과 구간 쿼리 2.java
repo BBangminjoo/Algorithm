cabe962f577339e6378ed6acbf0bfd0e2dd88e90
BBangminjoo <165381002+BBangminjoo@users.noreply.github.com>
2024-07-01 14:13:13 +0900

    Update 수열과 구간 쿼리 2.java

--- programmers/0레벨/수열과 구간 쿼리 2.java
+++ programmers/0레벨/수열과 구간 쿼리 2.java
@@ -5,7 +5,10 @@ class Solution {
     		int[] query = queries[i];
     		int currentNum = 1000001;		
     		for(int j = query[0]; j <= query[1]; j++) {
-    			if(arr[j] > query[2] && arr[j] < currentNum) currentNum = arr[j];
+    			if(arr[j] > query[2] && arr[j] < currentNum) {
+                    currentNum = arr[j];
+                }
+                    
     		}
     		answer[i] = (currentNum == 1000001) ?  -1 : currentNum;
     	}
