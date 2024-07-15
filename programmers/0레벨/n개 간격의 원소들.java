4b838c1842ae13a6d8195666ffbddc10fd399ebe
BBangminjoo <165381002+BBangminjoo@users.noreply.github.com>
2024-07-15 11:04:24 +0900

    Create n개 간격의 원소들.java

---
+++ programmers/0레벨/n개 간격의 원소들.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+		List list = new ArrayList();
+		for(int i=0; i<num_list.length; i+=n) {
+			list.add(num_list[i]);
+		}
+		
+		answer = new int[list.size()];
+		for(int j=0; j<answer.length; j++) {
+			answer[j] = (int)list.get(j);
+		}        
+        return answer;
+    }
+}
