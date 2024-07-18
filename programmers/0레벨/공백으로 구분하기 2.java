61b9f89d2236d50985afe439656da607879d4ffe
BBangminjoo <165381002+BBangminjoo@users.noreply.github.com>
2024-07-18 12:48:22 +0900

    Create 공백으로 구분하기 2.java

---
+++ programmers/0레벨/공백으로 구분하기 2.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.trim().split("\\s+"); //  "\\s+"를 이용하면 공백, 탭 등의 문자가 하나 이상 있을 시에 묶어서 split 해줌
+
+        return answer;
+    }
+}
