2a4928c648fc43144216b4622e91e95fb1004e35
BBangminjoo <165381002+BBangminjoo@users.noreply.github.com>
2024-07-26 20:05:15 +0900

    Create 꼬리 문자열.java

---
+++ programmers/0레벨/꼬리 문자열.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+		String[] answer = {};
+		List list = new ArrayList();
+		for(int i=0; i<str_list.length; i++) {
+			if(!str_list[i].contains(ex)) {
+				list.add(str_list[i]);
+			}
+		}
+		
+		answer = new String[list.size()];
+		for(int i=0; i<answer.length; i++) {
+			answer[i] = (String)list.get(i);
+		}
+		String result = String.join("",answer); 
+        return result;
+    }
+}
