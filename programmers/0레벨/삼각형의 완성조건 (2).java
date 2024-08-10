5361a9a2a90702c23f9664668e5ccae625c0ba96
BBangminjoo <165381002+BBangminjoo@users.noreply.github.com>
2024-08-10 15:13:29 +0900

    Update 삼각형의 완성조건 (2).java

--- programmers/0레벨/삼각형의 완성조건 (2).java
+++ programmers/0레벨/삼각형의 완성조건 (2).java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int[] sides) {
-        int answer = 0;
+방법1)	
+	    int answer = 0;
 		int max = Math.max(sides[0], sides[1]); // 0번째랑 1번째 배열 값중 최대값 찾기
 		int min = Math.min(sides[0], sides[1]); // 최소값
 		
@@ -11,3 +12,19 @@ class Solution {
         return answer;
     }
 }
+
+------------------------------------------------------
+방법2)	
+	int answer = 0;
+		int num = sides[0] + sides[1]; // 나머지 한 변을 지칭하는 변수
+		int max = Math.max(sides[0], sides[1]); // 0번째랑 1번째 배열 값중 최대값 찾기
+		int min = Math.min(sides[0], sides[1]); // 최소값
+
+		/* 1. 나머지 한 변이 주어진 배열 안에 있는 경우 */
+		for (int j = max-min+1; j <= max; j++) { // 최소값 다음부터 최대값(포함)까지
+				answer ++;				
+		}
+		/* 2. 나머지 한 변이 배열에 없는 경우 */
+		for (int k = max + 1; k < num; k++) {// 최대값부터 최대값+최솟값(미포함)까지
+			answer ++;
+		}
