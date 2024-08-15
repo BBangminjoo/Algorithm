e51502767a749081690a968f3f244863c24cdc79
BBangminjoo <165381002+BBangminjoo@users.noreply.github.com>
2024-08-15 23:27:36 +0900

    Update 로그인 성공?.java

--- programmers/0레벨/로그인 성공?.java
+++ programmers/0레벨/로그인 성공?.java
@@ -14,3 +14,24 @@ class Solution {
         return answer;
     }
 }
+-------------------------------------------------------------------------------
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+		for (int i = 0; i < db.length; i++) {
+			// id_pw[0]만 같을 때
+			if (id_pw[0].equals(db[i][0]) && !id_pw[1].equals(db[i][1])) {
+				answer = "wrong pw";
+				break;
+			}
+			// id_pw[0],[1]모두 같을 때
+			if (id_pw[0].equals(db[i][0]) && id_pw[1].equals(db[i][1])) {
+				answer = "login";
+				// id_pw[0],[1]모두 다를 때
+			} else {
+				answer = "fail";
+			}
+		}        
+        return answer;
+    }
+}	
