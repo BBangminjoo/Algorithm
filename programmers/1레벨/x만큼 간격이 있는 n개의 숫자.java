c518493d90e63c946555d17ebfd22d5aca87328a
BBangminjoo <165381002+BBangminjoo@users.noreply.github.com>
2024-08-26 00:04:05 +0900

    Update x만큼 간격이 있는 n개의 숫자.java

--- programmers/1레벨/x만큼 간격이 있는 n개의 숫자.java
+++ programmers/1레벨/x만큼 간격이 있는 n개의 숫자.java
@@ -2,16 +2,18 @@ import java.util.*;
 
 class Solution {
     public long[] solution(int x, int n) {
+		long sum=0;
 		long[] answer = new long[n];
 		
 		List list = new ArrayList();
 		
 		
-		for(int i=x;i<1000;i+=x) {
-			list.add(i);
+		for(int i=0;i<n;i++) {
+			sum+=x;
+			list.add(sum);
 		}
 		for(int j=0;j<answer.length;j++) {
-			answer[j] = (int)list.get(j);
+			answer[j] = (long)list.get(j);
 		}
         return answer;
     }
