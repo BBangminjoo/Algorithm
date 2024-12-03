7dffa840329795f9e15d2fc0e05f71a19e7e5dc6
민주 <165381002+BBangminjoo@users.noreply.github.com>
2024-12-03 10:49:50 +0900

    Update 나누어 떨어지는 숫자 배열.java

--- programmers/1레벨/나누어 떨어지는 숫자 배열.java
+++ programmers/1레벨/나누어 떨어지는 숫자 배열.java
@@ -1,1 +1,23 @@
-
+// 나누어 떨어지는 숫자 배열
+// array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환
+// divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+		List<Integer> list = new ArrayList();
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]%divisor==0) {
+				list.add(arr[i]);
+			}
+		}
+		if(list.size()==0) {
+			list.add(-1);
+		}
+	    Collections.sort(list);
+		int[] answer = new int[list.size()];
+		for(int i=0;i<answer.length;i++) {
+			answer[i] += list.get(i);
+		}
+        return answer;
+    }
+}
