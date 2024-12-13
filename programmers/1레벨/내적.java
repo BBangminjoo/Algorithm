89cc3b1a2b81b52e930ceec29d6a6c5948d565f4
민주 <165381002+BBangminjoo@users.noreply.github.com>
2024-12-13 14:47:45 +0900

    Update 내적.java

--- programmers/1레벨/내적.java
+++ programmers/1레벨/내적.java
@@ -1,3 +1,8 @@
+/*
+ * 길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다.
+ *  a와 b의 내적을 return
+ *  이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1]
+*/
 class Solution {
     public int solution(int[] a, int[] b) {
 		int answer = 0;
